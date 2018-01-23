d203080e7f9b760152743bb7619c12f7b3289cea
Krizthian <cristianortizmorales2013@gmail.com>
2018-01-23 14:44:43 -0500

    Revert "Fix fastcharge"
    This reverts commit ddeb04ffd14c57d95a0d50da559bd16528cc67c1.

--- kernel_build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export CROSS_COMPILE=/home/krizthian/android/kernel/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-UB-4.8/bin/arm-linux-androideabi-
-export ARCH=arm && export SUBARCH=arm
-make clean
-make mrproper
-make rebirth_jf_defconfig
-make -s -j8
-
