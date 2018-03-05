8d458b78d97cf98fac88cd07d20b2fb4b9436a6c
Krizthian <cristianortizmorales2013@gmail.com>
2018-03-04 21:37:09 -0500

    Revert "security: force SELinux permissive mode"
    This reverts commit d414fcc7c9ae740c5650d44349524a0906630d17.

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -107,13 +107,13 @@ extern struct security_operations seclsm_Operations;
 static atomic_t selinux_secmark_refcount = ATOMIC_INIT(0);
 
 #ifdef CONFIG_SECURITY_SELINUX_DEVELOP
-int selinux_enforcing = 0;
+int selinux_enforcing;
 
 static int __init enforcing_setup(char *str)
 {
 	unsigned long enforcing;
 	if (!strict_strtoul(str, 0, &enforcing))
-		selinux_enforcing = 0;
+		selinux_enforcing = enforcing ? 1 : 0;
 	return 1;
 }
 __setup("enforcing=", enforcing_setup);
